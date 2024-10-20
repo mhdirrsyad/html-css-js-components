65545b113ed9e4d361ac5983d884ceaa8e085794
mhdirrsyad <mohammadirsyad02@gmail.com>
2024-10-20 12:48:54 +0700

    Animated Navigation Added

---
+++ 20.animated-navigation/app.js
@@ -0,0 +1,8 @@
+// toggle
+const toggle = document.querySelector("#toggle");
+// nav
+const nav = document.querySelector("#nav");
+
+toggle.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
